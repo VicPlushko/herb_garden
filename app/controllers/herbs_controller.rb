4836ab2aa5e6baf670d0f89e661689c1b0bf2802
github username <github email address>
2020-06-29 14:33:32 -0400

    Adds edit route and view, adds delete route and button

--- app/controllers/herbs_controller.rb
+++ app/controllers/herbs_controller.rb
@@ -10,6 +10,24 @@ class HerbsController < ApplicationController
       end
 
       post '/herbs' do
+        herb = Herb.new(
+          name: params[:name],
+          latin_name: params[:latin_name],
+          family: params[:family],
+          medicinal_parts: params[:medicinal_parts],
+          taste: params[:taste],
+          energetics: params[:energetics],
+          actions: params[:actions],
+          ideal_for: params[:ideal_for],
+          applications: params[:applications],
+          dosage: params[:dosage],
+          considerations: params[:considerations]
+          )
+          if herb.save
+            redirect "/herbs/#{herb.id}"
+          else
+            redirect '/herbs/new'
+          end
       end
 
       get '/herbs/:id' do
@@ -21,4 +39,41 @@ class HerbsController < ApplicationController
             redirect '/herbs'
         end
       end
+
+      get '/herbs/:id/edit' do
+        @herb = Herb.find_by_id(params[:id])
+        erb :'/herbs/edit'
+      end
+
+      patch '/herbs/:id' do
+        herb = Herb.find_by_id(params[:id])
+        
+        if herb.update(
+          name: params[:name],
+          latin_name: params[:latin_name],
+          family: params[:family],
+          medicinal_parts: params[:medicinal_parts],
+          taste: params[:taste],
+          energetics: params[:energetics],
+          actions: params[:actions],
+          ideal_for: params[:ideal_for],
+          applications: params[:applications],
+          dosage: params[:dosage],
+          considerations: params[:considerations]
+          )
+          redirect "/herbs/#{herb.id}"
+        else
+          redirect "/herbs/#{herb.id}/edit"
+        end
+      end
+
+      delete '/herbs/:id' do
+        herbs = Herb.find_by_id(params[:id])
+        if herb
+          herb.delete
+        end
+        redirect '/herbs'
+      end
+
+
 end
--- app/models/herb.rb
+++ app/models/herb.rb
@@ -1,3 +1,4 @@
 class Herb < ActiveRecord::Base
     belongs_to :user
+    validates :name, :latin_name, :family, :medicinal_parts, :taste, :energetics, :actions, :ideal_for, :applications, :dosage, :considerations, presence: true
 end
