af6b3817d83d7d3b7549d7d5a00fd69903fc1256
github username <github email address>
2020-07-11 04:04:09 -0400

    changes email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,10 @@ class User < ActiveRecord::Base
   has_many :herbs
   has_secure_password
   validates :name, :email, :password, presence: true
-  validates :email, 
-  format: { with: /A[^@s] @[^@s] z/, message: "Email invalid"  },
-  uniqueness: true,
-  length: { minimum: 4, maximum: 254 }  
+  validates :email, uniqueness: true  
 
-  def self.is_email_valid?(email)
-    @email =~ /A[^@s] @[^@s] z/
-  end
+  # def self.is_email_valid?(email)
+  #   @email =~ /A[^@s] @[^@s] z/
+  # end
     
 end
